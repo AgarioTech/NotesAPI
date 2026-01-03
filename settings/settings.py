586637acbc8dfd446023bd79276d3325c13efff5
denischepik <gromovdenis706@gmail.com>
2026-01-03 20:29:08 +0300

    change settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -124,17 +124,17 @@ if os.environ.get("Django CI"):
         }
     }
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT')
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT')
+        }
     }
-}
 
 
 # Password validation
