98fd5396a6ce921c679631b72b37ef075c315931
denischepik <gromovdenis706@gmail.com>
2026-01-03 19:50:35 +0300

    add check db in settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -114,6 +115,15 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+if os.environ.get("Django CI"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 DATABASES = {
     'default': {
